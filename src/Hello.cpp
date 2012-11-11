3867ae58d48383436b3fa108c74141ddd204e711
bahasdu <bahasdu@gmail.com>
2012-11-11 13:16:45 +0600

    changes added

--- src/Hello.cpp
+++ src/Hello.cpp
@@ -3,17 +3,16 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-int main( int argc, char** argv )
-{
-Mat image;
-image = imread( argv[1], 1 );
-if( argc != 2 || !image.data )
-{
-printf( "No image data \n" );
-return -1;
-}
-namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
-imshow( "Display Image", image );
-waitKey(0);
-return 0;
+int main(int argc, char** argv) {
+	Mat image;
+	image = imread(argv[1], 1);
+	if (argc != 2 || !image.data) {
+		printf("No image data \n");
+		return -1;
+	}
+	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+	imshow("Display Image", image);
+	cout<<"hello world"<<endl;
+	waitKey(0);
+	return 0;
 }
